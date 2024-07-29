416beb3b984f41c39d85196dc2e26927587a518a
Nishant-kumar-yadav-10 <nishant12ts@gmail.com>
2024-07-29 04:55:58 +0000

    array

---
+++ 01-basics/Array.js
@@ -0,0 +1,21 @@
+//const marvel_heros=["thor","superman","flash"]
+//const dc_heros=["spiderman","batman"]
+
+//marvel_heros.push(dc_heros)
+//console.log(marvel_heros);
+//console.log(marvel_heros[3][1])
+//const allheros=marvel_heros.concat(dc_heros)
+//console.log(allheros);
+//const all_new_heros=[...marvel_heros,...dc_heros]
+//console.log(all_new_heros);
+//const another_array=[1,2,3,[3,4,5],6,7,8,[9,10,[1,2,3]]]
+//const real_another_array=another_array.flat(Infinity)
+//console.log(real_another_array);
+//console.log(Array.isArray("nishant"));
+//console.log(Array.from("nishant"));
+//console.log(Array.from({name:"nishant"}));
+//const score1=2
+//const score2=3
+//const score3=9
+
+//console.log(Array.of(score1,score2,score3));
