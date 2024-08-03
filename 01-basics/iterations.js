04921e152cea5d47e434e4eed8866c8a964c232d
Nishant-kumar-yadav-10 <nishant12ts@gmail.com>
2024-08-03 12:25:31 +0000

    nishant

--- 01-basics/iterations.js
+++ 01-basics/iterations.js
@@ -100,4 +100,23 @@ nishant.forEach((ites)=>{
 
 const arr=[1,2,3,4,5,6,7,8,9]
 const newarr=arr.filter( (ites)=> ites>6)
-console.log(newarr);
+//console.log(newarr);
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  //let userbooks=books.filter( (ites)=> ites.genre==="Fiction")
+  //console.log(userbooks);
+  let userbooks=books.filter( (ites)=> {
+    return ites.publish<=2000 && ites.genre==="Non-Fiction"
+  })
+  console.log(userbooks);
